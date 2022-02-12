be233660946f117b715ae8b9a42e0de97725b47e
JakubM3008 <mroczekjakub11@gmail.com>
2022-02-12 13:47:00 +0000

    miscell

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(
+    options: const FirebaseOptions(
       apiKey: "XXX",
       appId: "XXX",
       messagingSenderId: "XXX",
