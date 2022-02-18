a79f8f30da051120df1fa5a8336d551ed66cc394
Lsfhv <Lsfhv@protonmail.com>
2022-02-18 20:35:10 +0000

    refactor into form

--- lib/screens/authenticate/sign_up.dart
+++ lib/screens/authenticate/sign_up.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bike_tour_app/screens/authenticate/sign_up_form.dart';
 import 'package:bike_tour_app/services/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -31,64 +32,7 @@ class _SignUpState extends State<SignUp> {
       appBar: AppBar(
         title: const Text("Sign up"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: "First name",
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: "Last name",
-                  ),
-                ),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                  ),
-                ),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                  ),
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Confirm Password",
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  width: 250,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: TextButton(
-                    onPressed: () async {
-                      context.read<AuthService>().signUp(
-                            email: emailController.text.trim(),
-                            password: passwordController.text,
-                          );
-                    },
-                    child: const Text(
-                      'Register',
-                      style: TextStyle(color: Colors.white, fontSize: 25),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+      body: SignUpForm(),
     );
   }
 }
