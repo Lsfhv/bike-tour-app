8038fb5ce45b5320a2f52666ccc5ff384b933941
Lsfhv <Lsfhv@protonmail.com>
2022-02-18 20:30:07 +0000

    bloat ware

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,14 +1,5 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:bike_tour_app/main.dart';
 import 'package:bike_tour_app/screens/authenticate/sign_in_form.dart';
-import 'package:bike_tour_app/screens/navigation/main_map.dart';
-import 'package:bike_tour_app/screens/authenticate/sign_up.dart';
-import 'package:bike_tour_app/services/auth.dart';
-// import 'package:bike_tour_app/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -21,10 +12,6 @@ class _SignInState extends State<SignIn> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  // final AuthService _auth = AuthService();
-
-  final _auth = FirebaseAuth.instance;
-
   @override
   void dispose() {
     emailController.dispose();
@@ -34,13 +21,12 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    // print(_auth.toString());
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Login Page"),
+        title: const Text("Login Page"),
       ),
-      body: SignInForm(),
+      body: const SignInForm(),
     );
   }
 }
