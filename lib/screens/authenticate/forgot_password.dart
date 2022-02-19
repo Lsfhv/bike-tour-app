5fa0369f3dca60d536dd2ec91f26f67f08b9f978
Lsfhv <Lsfhv@protonmail.com>
2022-02-19 14:52:08 +0000

    create the scrren for forgot password '

---
+++ lib/screens/authenticate/forgot_password.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordState createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Sign up"),
+      ),
+      body: const Text("forgot password"),
+    );
+  }
+}
