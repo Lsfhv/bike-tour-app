480f4b952108e1973ff50980843c676b97cf6e0b
Lsfhv <Lsfhv@protonmail.com>
2022-02-22 18:56:56 +0000

    query database

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,4 +1,5 @@
 import 'package:bike_tour_app/services/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,19 +13,26 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
 
   String? _email;
+  String? _uid; 
   String? _firstName;
   String? _lastName;
-  // TODO retrieve data from cloud firestore
 
-
-  void generateFields() {
+  void generateFields() async {
     var currentUser = context.read<AuthService>().currentUser; 
 
     _email = currentUser!.email;
+    _uid = currentUser.uid;
+
+    var userdata = await FirebaseFirestore.instance.collection('users')
+      .doc(_uid).get();
+
+    _firstName = userdata.data()!['firstName'];
+    _lastName = userdata.data()!['lastName'];
   }
 
   @override
   Widget build(BuildContext context) {
+    generateFields();
     
     return Scaffold(
       appBar: AppBar(
