b5501bb8b0968865f1bdc75bf6775a3c350c2279
Lsfhv <Lsfhv@protonmail.com>
2022-02-22 17:19:31 +0000

    get data from firestore

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,4 +1,6 @@
+import 'package:bike_tour_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({ Key? key }) : super(key: key);
@@ -9,10 +11,21 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
 
-  
+  String? _email;
+  String? _firstName;
+  String? _lastName;
+  // TODO retrieve data from cloud firestore
+
+
+  void generateFields() {
+    var x = context.read<AuthService>().currentUser; 
+
+    _email = x!.email;
+  }
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
