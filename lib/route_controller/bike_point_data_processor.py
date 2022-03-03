55b04836c5f71f2fc14680af7ef3e10318d23241
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2022-03-03 03:04:35 +0000

    Update bike_point_data_processor.py
    bike availability and bike spaces are now pulled from API

--- lib/route_controller/bike_point_data_processor.py
+++ lib/route_controller/bike_point_data_processor.py
@@ -3,80 +3,87 @@ import json
 import requests
 import googlemaps
 
+
 class BikeDataPull:
-    def __init__(self, url = "https://api.tfl.gov.uk/BikePoint/" , points = None):
+    def __init__(self, url="https://api.tfl.gov.uk/BikePoint/", points=None, bikeAvailable=None, bikeSpace=None):
         self.url = url
+
         if not points:
             points = {}
         self.points = points
-        
+
+        if not bikeAvailable:
+            bikeAvailable = {}
+        self.bikeAvailable = bikeAvailable
+
+        if not bikeSpace:
+            bikeSpace = {}
+        self.bikeSpace = bikeSpace
+
     def pull_data(self):
         try:
             response = requests.get(self.url)
-            datas =  json.loads(response.text)
+            datas = json.loads(response.text)
             for data in datas:
-                common_names, lat, lon = data['commonName'], data['lat'], data['lon'] 
+                common_names, lat, lon, bike_available, bike_space = data['commonName'], data['lat'], data[
+                    'lon'], data['additionalProperties'][6]['value'], data['additionalProperties'][7]['value']
                 self.points[common_names] = (lat, lon)
+                self.bikeAvailable[common_names] = (bike_available)
+                self.bikeSpace[common_names] = (bike_space)
         except requests.ConnectionError:
-           print("error") 
-    
-    # def pull_test(self):
-
-    #     data = json.loads(requests.get(self.url).text)
-    #     print(type(data))
+            print("error")
 
 
 class BikeDataProcessor:
     limit = 500
     client = googlemaps.Client(key="AIzaSyDN4RDUVv8lX81W1CeoqKVIAObUdAA0mQI")
-    #parameters are coordinates
-    
+    # parameters are coordinates
+
     @staticmethod
     def postcode(latlng):
-        address = client.reverse_geocode(latlng = latlng)
+        address = client.reverse_geocode(latlng=latlng)
         postcode = address[0]['address_components'][-1]['long_name']
         return postcode
-    
+
     @staticmethod
     def map_postcode_to_lanlng(data):
         mappings = {}
-        for d in data.keys(): 
+        for d in data.keys():
             mappings[BikeDataProcessor.postcode(d)] = d
-    
+
     @staticmethod
     def calculate_distance(origin, destination):
         try:
-            response = BikeDataProcessor.client.distance_matrix(origin,destination, mode='walking', units = "metric", avoid = 'highways')
+            response = BikeDataProcessor.client.distance_matrix(
+                origin, destination, mode='walking', units="metric", avoid='highways')
             dist = response['rows'][0]['elements'][0]['distance']['value']
             return dist
             #data = json.load(response)
             #distance = data['rows']['elements']['distance']
-            #return distance
-        except googlemaps.exceptions.ApiError :
+            # return distance
+        except googlemaps.exceptions.ApiError:
             print("error with API key")
-    
-    
+
     @staticmethod
-    def filter_based_on_distance(origin,limit, data):
+    def filter_based_on_distance(origin, limit, data):
         points_within_range = list()
         for d in data.keys():
             destination = data[d]
-            if(BikeDataProcessor.calculate_distance(origin,destination) > limit):
+            if(BikeDataProcessor.calculate_distance(origin, destination) > limit):
                 points_within_range.append(destination)
         return destination
+
 
 if __name__ == "__main__":
     client = googlemaps.Client(key="AIzaSyDN4RDUVv8lX81W1CeoqKVIAObUdAA0mQI")
     b = BikeDataPull()
     b.pull_data()
-    # b.pull_test()
-    
-    
-    curr_coord = (51.5044584,-0.105681) #test
-    BikeDataProcessor.postcode(curr_coord)
-    #destination = b.points['Tanner Street, Bermondsey']
-    #destination = (51.5007,0.0858)
-    #a = BikeDataProcessor.calculate_distance(curr_coord,destination)
-    #print(a)
-    #list = BikeDataProcessor.filter_based_on_distance(curr_coord,500,b.points)
-    #print(list)
+
+    # curr_coord = (51.5044584,-0.105681) #test
+    # BikeDataProcessor.postcode(curr_coord)
+    # destination = b.points['Tanner Street, Bermondsey']
+    # destination = (51.5007,0.0858)
+    # a = BikeDataProcessor.calculate_distance(curr_coord,destination)
+    # print(a)
+    # list = BikeDataProcessor.filter_based_on_distance(curr_coord,500,b.points)
+    # print(list)
