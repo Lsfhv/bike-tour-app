e7d4df814ff9f5354b6d7ac80db82968a55982b8
lsfhv <Lsfhv@protonmail.com>
2022-03-21 19:47:50 +0000

    some sign up form tests

--- test/screens/authenticate/sign_in_test.dart
+++ test/screens/authenticate/sign_in_test.dart
@@ -1,11 +1,19 @@
 import 'package:bike_tour_app/screens/authenticate/sign_in_form.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:bike_tour_app/screens/navigation/to_page.dart';
+import 'package:bike_tour_app/screens/wrapper.dart';
+import 'package:bike_tour_app/services/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:bike_tour_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 
-    // await tester.pumpWidget(const SignInForm());
-    
-    assert(1==1);
   });
 }
--- test/screens/authenticate/sign_up_form_test.dart
+++ test/screens/authenticate/sign_up_form_test.dart
@@ -1,1 +1,60 @@
+import 'package:bike_tour_app/screens/authenticate/sign_up_form.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Testing that the text form fields in the sign up page actually exist', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: Scaffold(body: SignUpForm(),),));
+    
+    var firstNameField = find.byKey(const Key("FirstNameField"));
+    expect(firstNameField, findsOneWidget);
+
+    var lastNameField = find.byKey(const Key("LastNameField"));
+    expect(lastNameField, findsOneWidget);
+
+    var emailField = find.byKey(const Key("EmailField"));
+    expect(emailField, findsOneWidget);
+
+    var passwordField = find.byKey(const Key("PasswordField"));
+    expect(passwordField, findsOneWidget);
+
+    var passwordConfirmField = find.byKey(const Key("PasswordConfirmField"));
+    expect(passwordConfirmField, findsOneWidget);
+
+  });
+
+
+  testWidgets("Test that the parent form exists", (WidgetTester tester) async {
+    // await tester.pumpWidget(const MaterialApp(home: Scaffold(body: SignUpForm(),),));
+
+    // var form = find.byKey(const Key("Form"));
+    // expect(form, findsOneWidget);
+
+
+    // FIND OUT A WAY TO RETRIEVE _FORMKEY HERE!!!!!!!!!!KJASKJDFAJKSDLFHALSDFIKHKJFSDHUK
+
+    assert(0==0);
+  });
+
+  testWidgets("The text form fields are fillable", (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: Scaffold(body: SignUpForm(),),));
+
+    // retreive the text form fields like how its done in the ifrst test, fill them and test that it has indeed been filled 
+
+
+
+    assert(0==0);
+  });
+
+  testWidgets("Sign up form plays nicely with firebase", (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: Scaffold(body: SignUpForm(),),));
+
+    // enter the data into fake_firebase database, read the link below
+    // https://firebase.flutter.dev/docs/testing/testing/
+
+
+    assert(0==0);
+  });
+
+
+}
