53d9ca96d73b53b40d4de32c32b9da74cf3ab67a
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2022-03-25 08:01:01 +0000

    Update postcode.py

--- lib/models/postcode-api/postcode.py
+++ lib/models/postcode-api/postcode.py
@@ -1,29 +1,26 @@
 import json
 import requests
 import googlemaps
+import os
 
 
 class PostCodePull:
-    def __init__(self, url="https://api.tfl.gov.uk/BikePoint/", coords=None):
+    def __init__(self, url="https://api.tfl.gov.uk/BikePoint/"):
         self.url = url
-        if not coords:
-            coords = {}
-        self.coords = coords
 
     def pullLatlon(self):
-        outfile = open(
-            r"bike-tour-app/lib/models/postcode-api/latlon.txt", "r+")
-        try:
-            response = requests.get(self.url)
-            datas = json.loads(response.text)
-            for data in datas:
-                lat, lon = data['lat'], data['lon']
-                self.coords = lat, lon
-                # print(self.coords)
-                stringCoords = str(self.coords)
-                outfile.write(stringCoords + "\n")
-        except requests.ConnectionError:
-            print("Connection Error")
+        with open(r"bike-tour-app/lib/models/postcode-api/latlon.txt", "w") as outfile:
+            try:
+                response = requests.get(self.url)
+                datas = json.loads(response.text)
+                for data in datas:
+                    lat, lon = data['lat'], data['lon']
+                    latlon = str(lat) + ", " + str(lon)
+                    outfile.write(latlon + "\n")
+            except requests.ConnectionError:
+                print("Connection Error")
+            remove_chars = len(os.linesep)
+            outfile.truncate(outfile.tell() - remove_chars)
 
     def coordstxtToPost(self):
         with open(r"bike-tour-app/lib/models/postcode-api/latlon.txt", "r+") as file:
@@ -35,17 +32,21 @@ class PostCodePull:
             key="AIzaSyDN4RDUVv8lX81W1CeoqKVIAObUdAA0mQI")
         address = client.reverse_geocode(latlng=latlng)
         postcode = address[0]['address_components'][-1]['long_name']
-        # outfile = open(r"bike-tour-app/lib/models/postcode-api/postcode.txt", "r+")
-        # outfile.write(postcode + "\n")
-        return postcode
+        with open(r"bike-tour-app/lib/models/postcode-api/postcode.txt", "r+") as outfile:
+            outfile.write(postcode + "=> " + latlng + "\n")
+            remove_chars = len(os.linesep)
+            outfile.truncate(outfile.tell() - remove_chars)
 
-    def map_postcode_to_lanlng(data):
-        mappings = {}
-        for d in data.keys():
-            mappings[PostCodePull.postcode(d)] = d
-            print(d)
+    # def map_postcode_to_lanlng(data):
+    #     mappings = {}
+    #     for d in data.keys():
+    #         mappings[PostCodePull.pullPostCode(d)] = d
+
+    def sortPostCode():
+        pass
 
 
 if __name__ == "__main__":
-
-    PostCodePull.pullPostCode("51.487285, -0.217995")
+    obj = PostCodePull()
+    obj.pullLatlon()
+    obj.coordstxtToPost()
