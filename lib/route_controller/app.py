ce1d1e239c64bc7e8686add412158479ee2714ca
Berke Muftuoglu <berkemuftuoglu1@gmail.com>
2022-03-28 21:47:26 +0100

    trying to add points

--- lib/route_controller/app.py
+++ lib/route_controller/app.py
@@ -6,21 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def returnPoints():
-    json_file = {}
-    try:
-        datas = json.loads(requests.get("https://api.tfl.gov.uk/BikePoint/").text)
-        for data in datas:
-            common_names, lat, lon, bike_available, bike_space =  data['commonName'], \
-                                                                  data['lat'], \
-                                                                  data['lon'], \
-                                                                  data['additionalProperties'][6]['value'], \
-                                                                  data['additionalProperties'][7]['value']
-            arr = [lat, lon, str(bike_available), str(bike_space)]
-            json_file[common_names] = arr
-    except requests.ConnectionError:
-        print("error")
-    print(json_file)
-    return jsonify(json_file)
+    datas = json.loads(requests.get("https://api.tfl.gov.uk/BikePoint/").text)
+    print(datas)
+    return jsonify(datas)
 
 if __name__ == '__main__':
     app.run()
