80900843e84e19c4e8f6c01ad089ef100f68c068
Oluwamurewa Fagun <murewafagun@yahoo.co.uk>
2022-03-31 14:43:59 +0100

    signupp

---
+++ test/screens/Exception_Handler/if_no_wifi_test.dart
@@ -0,0 +1,15 @@
+import 'package:bike_tour_app/screens/Exception_Handler/if_no_wifi.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("Test", () {
+    testWidgets("Test1", (WidgetTester tester) async {});
+
+    testWidgets("Test2", (WidgetTester tester) async {});
+
+    testWidgets("Test3", (WidgetTester tester) async {});
+
+    testWidgets("Test4", (WidgetTester tester) async {});
+  });
+}
