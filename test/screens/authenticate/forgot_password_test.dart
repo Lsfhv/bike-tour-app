77c5ac743b536beafe6013e442f160a157439b5d
Shermiaya Raymond <shermaiyaraymond@gmail.com>
2022-04-04 13:43:44 +0100

    Fix tests

--- test/screens/authenticate/forgot_password_test.dart
+++ test/screens/authenticate/forgot_password_test.dart
@@ -1,22 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bike_tour_app/screens/authenticate/forgot_password_form.dart';
 
+
+import 'valid_regex.dart';
+
 void main() {
- 
+
+
   test('Empty email test', () {
-    final result = EmailFieldValidator.validate('');
+    
+    final result = ValidRegex().getValidEmailRegExp().hasMatch(' ');
 
     expect(result, 'Not a valid email');
   });
 
   test('Given invalid Email', () {
-    final result = EmailFieldValidator.validate('johnDoe@outlook');
+    final result = ValidRegex().getValidEmailRegExp().hasMatch('johnDoe@outlook');
 
     expect(result, 'Not a valid email');
   });
 
   test('Given valid Email', () {
-    final result = EmailFieldValidator.validate('johnDoe@outlook.com');
+    final result = ValidRegex().getValidEmailRegExp().hasMatch('johnDoe@outlook.com');
 
     expect(result, '');
   });
