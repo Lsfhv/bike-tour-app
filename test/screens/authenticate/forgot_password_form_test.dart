17501c202bf47014a7d165ee1b89258f32862bb5
Shermiaya Raymond <shermaiyaraymond@gmail.com>
2022-04-04 13:43:11 +0100

    Fix forgot password unit test

--- test/screens/authenticate/forgot_password_form_test.dart
+++ test/screens/authenticate/forgot_password_form_test.dart
@@ -4,26 +4,25 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("Test", () {
-    testWidgets("Test1", (WidgetTester tester) async {
+    testWidgets("Check widgets existence", (WidgetTester tester) async {
       await tester.pumpWidget(const MaterialApp(
           home: Scaffold(
         body: ForgotPasswordFrom(),
       )));
 
-      var email = find.byKey(const Key("EmailKey"));
-      expect(email, findsOneWidget);
+      var emailField = find.byKey(const Key("EmailKey"));
+      expect(emailField, findsOneWidget);
 
-      var reset = find.byKey(const Key("ResetKey"));
-      expect(reset, findsOneWidget);
+      var resetButton = find.byKey(const Key("ResetKey"));  
+      expect(resetButton, findsOneWidget);
 
-      await tester.enterText(email, r"""test@test.com""");
+      await tester.enterText(emailField, r"""test@test.com""");
       expect(find.text(r"""test@test.com"""), findsOneWidget);
 
-      // await tester.tap(reset);
-      // await tester.pump();
-
-      // expect(find.text('Please check your email to reset you password'),
-      //     findsOneWidget);
+      
+      // await tester.tap(resetButton);
+      // await tester.pumpAndSettle();
+      // expect(find.text('Please check your email to reset you password'), findsOneWidget);
     });
 
     testWidgets("Test2", (WidgetTester tester) async {});
