6fc0b710998c4644c22d4c46c2728617102f53a5
Oluwamurewa Fagun <murewafagun@yahoo.co.uk>
2022-04-05 09:15:42 +0100

    can't remember

---
+++ test/screens/authenticate/sign_up_form_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+main() {
+  test("title", () {
+
+    // setup
+
+    // run
+
+    // verify
+
+  });
+
+  test("Email is valid", () {
+
+    var result = 
+
+    // run
+
+    // verify
+
+  });
+}
