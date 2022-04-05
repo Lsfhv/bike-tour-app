3819d94cd67e88b5860a2b87319e501be6b37067
Shermiaya Raymond <shermaiyaraymond@gmail.com>
2022-04-05 08:54:25 +0100

    forgot passowrd test

--- test/screens/authenticate/forgot_password_form_test.dart
+++ test/screens/authenticate/forgot_password_form_test.dart
@@ -1,10 +1,28 @@
 import 'package:bike_tour_app/screens/authenticate/forgot_password_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:bike_tour_app/services/auth_service.dart';
+import 'package:mockito/mockito.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class MockUser extends Mock implements User{}
+
+final MockUser _mockUser = MockUser();
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {
+  @override
+  Stream<User> authStateChanges() {
+    return Stream.fromIterable([
+      _mockUser,
+    ]); //stream from iterable
+  }
+}
+
 
 void main() {
-  group("Test", () {
-    testWidgets("Check widgets existence", (WidgetTester tester) async {
+  group("Test", () {  
+    
+    testWidgets("Check widgets existence", (WidgetTester tester) async{
       await tester.pumpWidget(const MaterialApp(
           home: Scaffold(
         body: ForgotPasswordFrom(),
@@ -16,9 +34,8 @@ void main() {
       var resetButton = find.byKey(const Key("ResetKey"));  
       expect(resetButton, findsOneWidget);
 
-      await tester.enterText(emailField, r"""test@test.com""");
-      expect(find.text(r"""test@test.com"""), findsOneWidget);
-
+      await tester.enterText(emailField, "happy@example.com");
+      expect(find.text("happy@example.com"), findsOneWidget);
       
       // await tester.tap(resetButton);
       // await tester.pumpAndSettle();
